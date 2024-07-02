9709690a4eac9ab8b3390ec8f33dbd85a8926417
benleong0 <benleong@myurbanjungle.com>
2024-07-02 13:00:30 +0100

    binaryPairings

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -9,3 +9,16 @@ export function shuffleArr<T>(arr: T[], seed: number): T[] {
     .sort((a, b) => a.sort - b.sort)
     .map(({ value }) => value);
 }
+
+export function sum(arr: number[]): number {
+  return arr.reduce((a, b) => a + b);
+}
+
+export function* binaryPairings(length: number) {
+  for (let n = 0; n < Math.log2(length); n++) {
+    for (let l = 0; l < length; l += 2 ** (n + 1)) {
+      const r = l + 2 ** (n + 1);
+      yield { l, r };
+    }
+  }
+}
